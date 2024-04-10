7d3e08d7db140732a0729ca345f8c3a4ed748cb7
jhzcarvalho <jhcarvalho3@gmail.com>
2024-04-10 09:24:46 -0300

    ..

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,30 @@
-from flask import render_template
+from flask import render_template, request, redirect, flash
 from app import app
 
 
 @app.route("/")
-@app.route("/index")
-def index():
-    return render_template("index.html", nome="João")
+@app.route("/index", defaults={"nome": "usuário"})
+@app.route("/index/<nome>")
+def index(nome):
+    return render_template("index.html", nome=nome)
 
 
 @app.route("/contato")
 def contato():
     return render_template("contato.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/autenticar", methods=["POST"])
+def autenticar():
+    usuario = request.form.get("usuario")
+    senha = request.form.get("senha")
+    if usuario == "adm" and senha == "123":
+        return f"usuario: {usuario} e senha: {senha}"
+    else:
+        flash("Dados inválidos")
+        return redirect("/login")
